01016269c5cff69a07a6968880099d112b08fb56
JoNoC0 <jnguyeningco@gmail.com>
2023-04-21 16:44:41 -0500

    function to display notes with delete button

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ const noteList = document.getElementById('noteList');
 
 let notes = [];
 
+// eventListener to push notes to be displayed once addNote is clicked
 addNote.addEventListener('click', () => {
     if (note.value !== '') {
         notes.push(note.value);
@@ -12,3 +13,24 @@ addNote.addEventListener('click', () => {
     }
 });
 
+
+// creates notes with deleteButton when note is added
+function displayNotes() {
+    noteList.innerHTML = '';
+    notes.forEach((note, index) => {
+        const noteItem = document.createElement('div');
+        noteItem.classList.add('note');
+
+        const noteText = document.createElement('p');
+        noteText.innerText = note;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            deleteNote(index);
+        });
+        noteItem.appendChild(noteText);
+        noteItem.appendChild(deleteButton);
+        noteList.appendChild(noteItem);
+    });
+}
